0b32268ae1ca5056059165bfd4bc63a9909e6acc
Massyknight <arunrobert1998@gmail.com>
2021-09-15 23:12:46 -0700

    added the params

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/user',(req,res)=>{
     })
 })
 
+app.get('/user/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.json({
+        success:true,
+        message:'got one user',
+        users:req.params.id
+    })
+})
+
 app.listen(8000,()=>{
     console.log('server is running')
 })
