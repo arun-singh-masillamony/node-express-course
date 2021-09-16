8d6bb1d3b70655965d893f6442f22a8afeb16992
Massyknight <arunrobert1998@gmail.com>
2021-09-15 23:08:00 -0700

    added the json object

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express =require('express');
 const app= express();
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+]
+
+app.get('/user',(req,res)=>{
+    res.json({
+        Success:true,
+        message:'Successfully got the users. Nice!',
+        users:mockUserData
+    })
+})
+
 app.listen(8000,()=>{
     console.log('server is running')
 })
